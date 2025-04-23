0acf4eb05bcfa36f9b24478eff92258aa9cefd74
TylerOakes14 <oakes.tyler1441@gmail.com>
2025-04-23 20:33:16 +0000

    lab 16

--- Main.java
+++ Main.java
@@ -1,14 +1,40 @@
 /********************************************
-*	AUTHOR:	<name>
+*	AUTHOR:	<name> Tyler Oakes
 * COLLABORATORS: <names>
 *	COURSE:	CS 111 Intro to CS I - Java
-*	LAST MODIFIED:	<date>
+*	LAST MODIFIED:	<date> 04/22/25
 ********************************************/
 
 public class Main 
 {
   public static void main(String[] args)
   {
+      int[] values = {12, 16, 26,42, 53, 77, 84};
 
-  }
+      System.out.println("Test: array contents =" + ArrayMethods.arrayString(values));
+
+      ArrayMethods.swap(values, 0, 6);
+
+      System.out.println("Test: array after swap of 12 and 84 = " + ArrayMethods.arrayString(values));
+
+      int [] numbers = {42, 16, 84, 12, 77, 26, 53};
+      int minIndex1 = ArrayMethods.indexOfMin(numbers, 0);
+      //should be 12 (numbers[minIndex1]), index is 3
+      int minIndex2 = ArrayMethods.indexOfMin(numbers, 4);
+      //should be 26, index is 5
+
+      System.out.println("TEST: min indices should be 3 and 5, we got: " + minIndex1 + " and " + minIndex2);
+    
+      System.out.println("Test: array after reversal = " + ArrayMethods.arrayString(numbers));
+      ArrayMethods.reverse(numbers);
+
+      System.out.println("Test: array after reversal = " + ArrayMethods.arrayString(numbers));
+
+      System.out.println("Test: array before sorting = " + ArrayMethods.arrayString(numbers));
+
+      ArrayMethods.selectionSort(numbers);
+
+      System.out.println("Test: array after sorting = " + ArrayMethods.arrayString(numbers));
+
+    }
 }
